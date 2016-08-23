eb5c36f3cf3590812cc3858f5e736360ed1d3391
Enric Martinez <runlevel0@gmail.com>
2016-08-23 11:04:09 +0200

    New edit, new version for exercise 2.58 (crappy but it works) and reverted changes to the original

--- projects/chapter02/route-ticket-machine/TicketMachine.java
+++ projects/chapter02/route-ticket-machine/TicketMachine.java
@@ -17,7 +17,8 @@ public class TicketMachine
     private int balance;
     // The total amount of money collected by this machine.
     private int total;
-	
+    // route
+    private String route;
     /**
      * Create a machine that issues tickets of the given price.
      */
@@ -26,6 +27,7 @@ public class TicketMachine
         price = ticketCost;
         balance = 0;
         total = 0;
+        route = "A";
     }
 
     /**
@@ -44,7 +46,18 @@ public class TicketMachine
     {
         return balance;
     }
-	
+    
+    /** 
+    * Select route from A,B  and C
+    */
+
+    public String getRoute()
+    {
+       return route;
+    }
+
+   
+
     /**
      * Receive an amount of money in cents from a customer.
      * Check that the amount is sensible.
@@ -67,12 +80,27 @@ public class TicketMachine
      */
     public void printTicket()
     {
+        System.out.println("Please select Route: ");
+        System.out.println();
+         switch (route) {
+            case "A":
+                price = 12;
+                                //break;
+            case "B":
+                price = 14;
+                               //break;
+            case "C":
+                price = 10;
+                               //break;
+                            }
+
         if(balance >= price) {
             // Simulate the printing of a ticket.
             System.out.println("##################");
             System.out.println("# The BlueJ Line");
             System.out.println("# Ticket");
-			System.out.println("# " + price + " cents.");
+            System.out.println("# Route"+route);
+            System.out.println("# " + price + " cents.");
             System.out.println("##################");
             System.out.println();
 
